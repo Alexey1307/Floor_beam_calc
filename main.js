776444db2203c0cabeb62a254e4eaa97655b955a
alex <bachurin1307@gmail.com>
2020-11-11 20:05:13 +0300

    commit

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Привет, Vue!'
-  }
+/*<div id="app">
+	<nav>
+		<transition name='fade' mode='out-in'>
+			<i class="material-icons menu" v-if='!show' @click='show = !show' key='menu'>menu</i>
+			<i class="material-icons clear" v-else @click='show = !show' key='clear'>clear</i>
+		</transition>
+		<transition name='fade'>
+			<ul v-if='show'>
+				<li v-for='item in items'>{{item}}</li>
+			</ul>
+		</transition>
+	</nav>
+</div>*/
+
+/*new Vue({
+	el: '#app',
+	data:{
+		items: [
+			'Home',
+			'About'
+		],
+		show: false,
+	},
+});*/
+
+new Vue({
+	el: '#app',
+	data: {
+		show: true
+	}
 })
